521cce715474f6203ae891094015eb714d29e5ef
Wallace Lima <wallacereislima@gmail.com>
2019-09-07 12:20:15 -0300

    Ajustes

--- src/main/java/br/com/walldev/brewer/controller/CervejaController.java
+++ src/main/java/br/com/walldev/brewer/controller/CervejaController.java
@@ -2,11 +2,21 @@ package br.com.walldev.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.walldev.brewer.model.Cerveja;
 
 @Controller
 public class CervejaController {
-	@RequestMapping("/cervejas/novo")
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.GET)
 	public String criar() {
 		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println("Teste Post" + cerveja.getSku());
+		return null;
 	}
 }
